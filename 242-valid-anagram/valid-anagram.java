a9bdf8773d8a45cac40534bda72c9e3e6505c63b
Adil Feroze <115704572+Ash4477@users.noreply.github.com>
2025-07-21 01:41:53 +0500

    Time: 3 ms (91.49%) | Memory: 42.8 MB (97.53%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,7 +1,5 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        if (s.length() != t.length()) return false;
-
         int[] counter = new int[26];
 
         for (int i=0; i< s.length(); i++) {
