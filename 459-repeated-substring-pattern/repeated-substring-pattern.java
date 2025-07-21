fa3bab061fc3856705cab26a6a6580dde1958e27
Adil Feroze <115704572+Ash4477@users.noreply.github.com>
2025-07-22 03:03:00 +0500

    Time: 17 ms (80.75%) | Memory: 45.4 MB (39.45%) - LeetSync

--- 459-repeated-substring-pattern/repeated-substring-pattern.java
+++ 459-repeated-substring-pattern/repeated-substring-pattern.java
@@ -1,10 +1,9 @@
 class Solution {
     public boolean repeatedSubstringPattern(String s) {
-        StringBuilder subStr = new StringBuilder();
         int n = s.length();
-        for (int i=0; i<n/2; i++) {
-            subStr.append(s.charAt(i));
-            if (n % subStr.length() != 0) continue;
+        for (int i=n/2; i>=1; i--) {
+            if (n % i != 0) continue;
+            String subStr = s.substring(0, i);
             StringBuilder tempStr = new StringBuilder(subStr);
             while(tempStr.length() < n) {
                 tempStr.append(subStr);
