69ab8d485955948e4dfe29c2e4ef2b99d3668e50
Adil Feroze <115704572+Ash4477@users.noreply.github.com>
2025-07-25 04:23:36 +0500

    Time: 2 ms (99.84%) | Memory: 50.8 MB (9.38%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.java
+++ 229-majority-element-ii/majority-element-ii.java
@@ -1,18 +1,45 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        Map<Integer,Integer> map = new HashMap<>();
+        int majority1 = 0;
+        int majority2 = 0;
+        int count1 = 0;
+        int count2 = 0;
+
         int n = nums.length;
+        for (int i=0; i<n; i++) {
+            if (nums[i] == majority1) count1++;
+            else if (nums[i] == majority2) count2++;
+            else if (count1 == 0) {
+                majority1 = nums[i];
+                count1++;
+            }
+            else if (count2 == 0) {
+                majority2 = nums[i];
+                count2++;
+            }
+            else {
+                count1--;
+                count2--;
+            }
+        }
+
+        count1 = 0;
+        count2 = 0;
 
         for (int i=0; i<n; i++) {
-            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
+            if (nums[i] == majority1) count1++;
+            else if (nums[i] == majority2) count2++;
         }
 
         n = n/3;
         List<Integer> res = new ArrayList<>();
 
-        for (int i: map.keySet()) {
-            int count = map.get(i);
-            if (count > n) res.add(i);
+        if (count1 > n) {
+            res.add(majority1);
+        }
+
+        if (count2 > n) {
+            res.add(majority2);
         }
 
         return res;
