997b5986d5965ef1b56c2629198f1255c3bbacf0
Adil Feroze <115704572+Ash4477@users.noreply.github.com>
2025-07-25 00:31:29 +0500

    Time: 7 ms (38.70%) | Memory: 45.7 MB (27.20%) - LeetSync

--- 3206-find-common-elements-between-two-arrays/find-common-elements-between-two-arrays.java
+++ 3206-find-common-elements-between-two-arrays/find-common-elements-between-two-arrays.java
@@ -1,27 +1,26 @@
 class Solution {
     public int[] findIntersectionValues(int[] nums1, int[] nums2) {
-        HashMap<Integer, Integer> mp1 = new HashMap<>();
-        HashMap<Integer, Integer> mp2 = new HashMap<>();
-        int[] result = new int[2];
+       HashSet<Integer> set1 = new HashSet<>();
+       HashSet<Integer> set2 = new HashSet<>();
 
-        for (int i=0;i<nums1.length;i++) {
-            mp1.put(nums1[i], mp1.getOrDefault(nums1[i], 0) + 1);
-        }
+       for (int num: nums1) {
+        set1.add(num);
+       }
 
-        for (int i=0;i<nums2.length;i++) {
-            mp2.put(nums2[i], mp2.getOrDefault(nums2[i], 0) + 1);
-        }
+       for (int num: nums2) {
+        set2.add(num);
+       }
 
-        for (int num: mp1.keySet()) {
-            if(mp2.containsKey(num)) result[0] += mp1.get(num);
-        }
+        int count1 = 0;
+       for (int i=0;i<nums1.length;i++) {
+        if (set2.contains(nums1[i])) count1++;
+       }
 
-        for (int num: mp2.keySet()) {
-            if(mp1.containsKey(num)) result[1] += mp2.get(num);
-        }
+        int count2 = 0;
+       for (int i=0;i<nums2.length;i++) {
+        if (set1.contains(nums2[i])) count2++;
+       }
 
- 
-
-        return result;
+       return new int[] {count1, count2};
     }
 }
