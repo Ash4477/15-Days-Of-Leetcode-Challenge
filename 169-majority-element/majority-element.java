ca840af716d8dacf369ad8c58083c76932f6fbc4
Adil Feroze <115704572+Ash4477@users.noreply.github.com>
2025-07-25 03:53:19 +0500

    Time: 2 ms (73.81%) | Memory: 53.5 MB (13.83%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -3,9 +3,9 @@ class Solution {
         int count = 0;
         int candidate = 0;
 
-        for (int num: nums) {
-            if (count == 0) candidate = num;
-            if (num == candidate) count++;
+        for (int i=0; i<nums.length; i++) {
+            if (count == 0) candidate = nums[i];
+            if (nums[i] == candidate) count++;
             else count--;
         }
 
