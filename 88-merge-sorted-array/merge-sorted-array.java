c6d9739407761f88f9979bce53e39ae17c9a4a87
Adil Feroze <115704572+Ash4477@users.noreply.github.com>
2025-07-26 19:26:57 +0500

    Time: 0 ms (100.00%) | Memory: 42.3 MB (60.26%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.java
+++ 88-merge-sorted-array/merge-sorted-array.java
@@ -1,23 +1,18 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int[] res = new int[m+n];
-        int resIdx = 0, p1 = 0, p2 = 0;
+        int p = m+n-1, p1 = m-1, p2 = n-1;
 
-        while (p1 < m && p2 < n) {
-            if (nums1[p1] < nums2[p2]) res[resIdx++] = nums1[p1++];
-            else if (nums1[p1] > nums2[p2]) res[resIdx++] = nums2[p2++];
+        while (p1 >= 0 && p2 >= 0) {
+            if (nums1[p1] > nums2[p2]) nums1[p--] = nums1[p1--];
+            else if (nums1[p1] < nums2[p2]) nums1[p--] = nums2[p2--];
             else {
-                res[resIdx++] = nums1[p1++];
-                res[resIdx++] = nums2[p2++];
+                nums1[p--] = nums2[p2--];
+                nums1[p--] = nums1[p1--];
             }
         }
 
-        while (p1 < m) res[resIdx++] = nums1[p1++];
-
-        while (p2 < n) res[resIdx++] = nums2[p2++];
-
-        for (int j=0; j<resIdx; j++) {
-            nums1[j] = res[j];
+        while (p2>=0) {
+            nums1[p--] = nums2[p2--];
         }
     }
 }
