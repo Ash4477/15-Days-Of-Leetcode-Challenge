bf79b90420739265cdf97370943caa51c6ded370
Adil Feroze <115704572+Ash4477@users.noreply.github.com>
2025-07-28 12:55:21 +0500

    Time: 3 ms (72.90%) | Memory: 43.2 MB (70.08%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -1,25 +1,13 @@
 class Solution {
-    public boolean isAlphaNumeric(char c) {
-        if ((c >= 48 && c <=57) || (c >= 97 && c <=122)) return true;
-        else return false;
-    }
-
     public boolean isPalindrome(String s) {
-        int left = 0;   
-        int right = s.length() - 1;
+        int left = 0;
+        int right = s.length()-1;
         s = s.toLowerCase();
-
-        while(left < right) {
-            while (left < right && !isAlphaNumeric(s.charAt(left))) left++;
-            while (left < right && !isAlphaNumeric(s.charAt(right))) right--;
-
-            if (s.charAt(left) == s.charAt(right)) {
-                left++;
-                right--;
-            } else {
-                return false;
-            }
+        while (left < right) {
+           while (left < right && !Character.isLetterOrDigit(s.charAt(left))) left++;
+            while (left < right && !Character.isLetterOrDigit(s.charAt(right))) right--;
+            if (s.charAt(left++) != s.charAt(right--)) return false;
         }
         return true;
-    }   
+    }
 }
