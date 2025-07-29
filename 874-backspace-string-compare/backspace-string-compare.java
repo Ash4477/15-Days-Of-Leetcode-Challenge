b507c477b89481983e41f0da5e5b95a88b799ca6
Adil Feroze <115704572+Ash4477@users.noreply.github.com>
2025-07-29 20:37:02 +0500

    Time: 1 ms (79.09%) | Memory: 42.4 MB (7.40%) - LeetSync

--- 874-backspace-string-compare/backspace-string-compare.java
+++ 874-backspace-string-compare/backspace-string-compare.java
@@ -1,31 +1,25 @@
 class Solution {
     public boolean backspaceCompare(String s, String t) {
-        int i = s.length()-1, j = t.length()-1;
-        
-        while (i >=0 || j >= 0)  {
-            i = getNextValidChar(s, i);
-            j = getNextValidChar(t, j);
-
-            if (i<0 && j<0) return true;
-
-            if (i<0 || j<0) return false;
-
-            if (s.charAt(i) != t.charAt(j)) return false;
-
-            i--; j--;
-        }
-
-        return true;
+        String res1 = fn(s);
+        String res2 = fn(t);
+        return res1.equals(res2);
     }
 
-    public int getNextValidChar(String str, int end) {
-        int count = 0;
-        while (end>=0) {
-            if (str.charAt(end) == '#') count++;
-            else if (count > 0) count--;
-            else break;
-            end--;
+    public String fn(String str) {
+        Deque<Character> st = new ArrayDeque<>();
+        int n = str.length();
+        for (int i=0; i<n; i++) {
+            char c = str.charAt(i);
+            if (c != '#') st.push(c);
+            else {
+                if (!st.isEmpty()) st.pop();
+            }
         }
-        return end;
+        StringBuilder res = new StringBuilder();
+        while (!st.isEmpty()) {
+            res.append(st.pop());
+        }
+
+        return res.toString();
     }
 }
