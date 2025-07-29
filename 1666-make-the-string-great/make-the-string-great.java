7b84aeb02081d08793a70a76657883f113cc7635
Adil Feroze <115704572+Ash4477@users.noreply.github.com>
2025-07-29 21:51:50 +0500

    Time: 2 ms (80.68%) | Memory: 42 MB (89.43%) - LeetSync

--- 1666-make-the-string-great/make-the-string-great.java
+++ 1666-make-the-string-great/make-the-string-great.java
@@ -3,21 +3,16 @@ class Solution {
         StringBuilder res = new StringBuilder(s);
         int len = s.length();
         int i=1;
+
         while (i<len) {
-            if (check(res.toString(), i)) {
+            char c1 = res.charAt(i);
+            char c2 = res.charAt(i-1);
+            if ( (c1 == c2 + 32) || (c1 == c2-32) ) {
                 res.delete(i-1, i+1);
                 len = res.length();
                 i=1;
             } else i++;
         }
         return res.toString();
-        
-    }
-
-    public boolean check(String s, int i) {
-        char c1 = s.charAt(i);
-        char c2 = s.charAt(i-1);
-        if ( (c1 == c2 + 32) || (c1 == c2-32) ) return true;
-        return false;
     }
 }
