caacc9d2077dca148ecf300f23008d223157ff47
Adil Feroze <115704572+Ash4477@users.noreply.github.com>
2025-07-30 22:55:14 +0500

    Time: 0 ms (100.00%) | Memory: 41.2 MB (96.99%) - LeetSync

--- 957-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.java
+++ 957-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.java
@@ -1,12 +1,13 @@
 class Solution {
     public int minAddToMakeValid(String s) {
-        Deque<Character> st = new ArrayDeque<>();
+        int open = 0, close = 0;
+
         for (char c : s.toCharArray()) {
-            if (st.isEmpty()) st.push(c);
-            else if (st.peek() == '(' && c == ')') st.pop();
-            else st.push(c);
+            if (c == '(') open++;
+            else if (open > 0) open--;
+            else close++;
         }
 
-        return st.size();
+        return open+close;
     }
 }
