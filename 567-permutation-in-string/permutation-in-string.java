770f79d3c5640fc9b40b9fca93d581aca575fd5f
Adil Feroze <115704572+Ash4477@users.noreply.github.com>
2025-08-07 19:32:27 +0500

    Time: 4 ms (98.90%) | Memory: 42.9 MB (57.53%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.java
+++ 567-permutation-in-string/permutation-in-string.java
@@ -1,24 +1,26 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
-        if (s1.length() > s2.length()) return false;
+        int k = s1.length();
+        if (k > s2.length()) return false;
+
         int[] freq1 = new int[26];
         int[] freq2 = new int[26];
+        char[] str = s2.toCharArray();
         
-        int k = 0;
         for (char c : s1.toCharArray()) {
             freq1[c-'a']++;
-            k++;
         }
 
         for (int i=0; i<k; i++) {
-            freq2[s2.charAt(i) - 'a']++;
+            freq2[str[i] - 'a']++;
         }
 
         if (Arrays.equals(freq1, freq2)) return true;
 
-         for (int i=k; i<s2.length(); i++) {
-            freq2[s2.charAt(i) - 'a']++;
-            freq2[s2.charAt(i-k) - 'a']--;
+
+         for (int i=k; i<str.length; i++) {
+            freq2[str[i] - 'a']++;
+            freq2[str[i-k] - 'a']--;
             if (Arrays.equals(freq1, freq2)) return true;
         }
 
