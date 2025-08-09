caa5e0c045077de5c936776f8b0bb0d3fddc8b13
Adil Feroze <115704572+Ash4477@users.noreply.github.com>
2025-08-09 10:35:31 +0500

    Time: 19 ms (52.97%) | Memory: 45.5 MB (35.68%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.java
+++ 76-minimum-window-substring/minimum-window-substring.java
@@ -1,46 +1,39 @@
 class Solution {
-    public boolean check(String t, int[] freq) {
-        for (char c : t.toCharArray()) {
-            if (freq[c] > 0) return false;
-        }
-        return true;
-    }
-
     public String minWindow(String s, String t) {
-        if (s.length() < t.length()) return "";
+        if (s.length() == 0 || t.length() == 0) return "";
 
-        Map<Character, Integer> freq = new HashMap<>();
+        Map<Character, Integer> tCount = new HashMap<>();
         for (char c : t.toCharArray()) {
-            freq.put(c, freq.getOrDefault(c, 0) + 1);
+            tCount.put(c, tCount.getOrDefault(c, 0) + 1);
         }
 
-        int targetCharsRemaining = t.length();
-        int[] minWindow = {0, Integer.MAX_VALUE};
+        Map<Character, Integer> windowCount = new HashMap<>();
+        int have = 0, need = tCount.size();
         int left = 0;
+        int minLen = Integer.MAX_VALUE;
+        int resStart = 0;
 
-        for (int right=0; right<s.length(); right++) {
-            char ch = s.charAt(right);
-            if (freq.getOrDefault(ch, 0) > 0) targetCharsRemaining--;
-            freq.put(ch, freq.getOrDefault(ch, 0) - 1);
+        for (int right = 0; right < s.length(); right++) {
+            char c = s.charAt(right);
+            windowCount.put(c, windowCount.getOrDefault(c, 0) + 1);
 
-            if (targetCharsRemaining == 0) {
-                while (true) {
-                    char charAtStart = s.charAt(left);
-                    if (freq.getOrDefault(charAtStart, 0) == 0) break;
-                    freq.put(charAtStart, freq.getOrDefault(charAtStart, 0) + 1);
-                    left++;
+            if (tCount.containsKey(c) && windowCount.get(c).intValue() == tCount.get(c).intValue()) have++;
+
+            while (have == need) {
+                if (right - left + 1 < minLen) {
+                    minLen = right - left + 1;
+                    resStart = left;
                 }
-                if (right-left < minWindow[1] - minWindow[0]) {
-                    minWindow[0] = left;
-                    minWindow[1] = right;
-                }
-                 freq.put(s.charAt(left), freq.getOrDefault(s.charAt(left), 0) + 1);
-                targetCharsRemaining++;
+
+                char leftChar = s.charAt(left);
+                windowCount.put(leftChar, windowCount.get(leftChar) - 1);
+
+                if (tCount.containsKey(leftChar) && windowCount.get(leftChar).intValue()< tCount.get(leftChar).intValue()) have--;
+
                 left++;
-
             }
         }
-                return minWindow[1] >= s.length() ? "" : s.substring(minWindow[0], minWindow[1] + 1);        
 
+        return minLen == Integer.MAX_VALUE ? "" : s.substring(resStart, resStart + minLen);
     }
 }
