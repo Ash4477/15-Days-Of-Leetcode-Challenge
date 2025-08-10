9774c0cd6ce6cef0cc7eae628044b8110a648bb8
Adil Feroze <115704572+Ash4477@users.noreply.github.com>
2025-08-11 01:21:46 +0500

    Time: 0 ms (100.00%) | Memory: 47.7 MB (49.66%) - LeetSync

---
+++ 222-count-complete-tree-nodes/count-complete-tree-nodes.java
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int count;
+    public int countNodes(TreeNode root) {
+        count = 0;
+        fn(root);
+        return count;
+    }
+
+    public void fn(TreeNode root) {
+        if (root == null) return;
+        count++;
+        fn(root.left);
+        fn(root.right);
+    }
+}
