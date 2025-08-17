30af7542174dd1aa83328a3f8895861eddaf7ca8
Adil Feroze <115704572+Ash4477@users.noreply.github.com>
2025-08-17 23:51:43 +0500

    Time: 11 ms (91.88%) | Memory: 45.7 MB (88.57%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/top-k-frequent-elements.java
@@ -1,22 +1,27 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        Map<Integer,Integer> map = new HashMap<>();
-
-        for (int i : nums) {
-            map.put(i, map.getOrDefault(i, 0) + 1);
+        Map<Integer, Integer> freqMap = new HashMap<>();
+        for (int num : nums) {
+            freqMap.put(num, freqMap.getOrDefault(num, 0) + 1);
         }
+
+        List<Integer>[] buckets = new List[nums.length+1];
+        for (int key : freqMap.keySet()) {
+            int freq = freqMap.get(key);
+            if (buckets[freq] == null) buckets[freq] = new ArrayList<>();
+            buckets[freq].add(key);
+        }
+
         int[] result = new int[k];
-        for (int i=0; i<k; i++) {
-            int maxVal = Integer.MIN_VALUE;
-            for (Map.Entry<Integer,Integer> entry : map.entrySet()) {
-                if (maxVal == Integer.MIN_VALUE || entry.getValue() > map.get(maxVal)) {
-                    maxVal = entry.getKey();
+        int resIdx = 0;
+
+        for (int i = buckets.length - 1; i >= 0 && resIdx < k; i--) {
+            if (buckets[i] != null) {
+                for (int j = 0; j < buckets[i].size() && resIdx < k; j++) {
+                    result[resIdx++] = buckets[i].get(j);
                 }
             }
-            result[i] = maxVal;
-            map.remove(maxVal);
         }
-
         return result;
     }
 }
