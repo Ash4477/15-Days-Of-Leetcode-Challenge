c752deb731c0a382d7b65b4a095794e626f868cb
Adil Feroze <115704572+Ash4477@users.noreply.github.com>
2026-02-20 23:43:46 +0500

    Time: 1 ms (95.53%) | Memory: 43.5 MB (24.50%) - LeetSync

--- 1894-merge-strings-alternately/merge-strings-alternately.java
+++ 1894-merge-strings-alternately/merge-strings-alternately.java
@@ -1,17 +1,24 @@
 class Solution {
     public String mergeAlternately(String word1, String word2) {
-        int p1 = 0, p2 = 0, n1 = word1.length(), n2 = word2.length();
-        char[] res = new char[n1+n2];
-        int resIdx = 0;
+        char[] word1Arr = word1.toCharArray();
+        char[] word2Arr = word2.toCharArray();
+        int idx1 = 0;
+        int idx2 = 0;
+        StringBuilder sb = new StringBuilder();
 
-        while (p1 < n1 && p2 < n2) {
-            res[resIdx++] = word1.charAt(p1++);
-            res[resIdx++] = word2.charAt(p2++);
+        while (idx1 < word1Arr.length && idx2 < word2Arr.length) {
+            sb.append(word1Arr[idx1++]);
+            sb.append(word2Arr[idx2++]);
         }
 
-        while (p1<n1) res[resIdx++] = word1.charAt(p1++);
-        while (p2<n2) res[resIdx++] = word2.charAt(p2++);
+        while (idx1 < word1Arr.length) {
+            sb.append(word1Arr[idx1++]);
+        }
 
-        return new String(res);
+        while (idx2 < word2Arr.length) {
+            sb.append(word2Arr[idx2++]);
+        }
+
+        return sb.toString();
     }
 }
