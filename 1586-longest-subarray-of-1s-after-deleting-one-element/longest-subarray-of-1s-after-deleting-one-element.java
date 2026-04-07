ab2203f9a0153f798aa26820757d75628cb8ed78
Adil Feroze <115704572+Ash4477@users.noreply.github.com>
2026-04-07 20:50:23 +0500

    Time: 3 ms (87.32%) | Memory: 65.6 MB (37.27%) - LeetSync

--- 1586-longest-subarray-of-1s-after-deleting-one-element/longest-subarray-of-1s-after-deleting-one-element.java
+++ 1586-longest-subarray-of-1s-after-deleting-one-element/longest-subarray-of-1s-after-deleting-one-element.java
@@ -1,43 +1,24 @@
 class Solution {
     public int longestSubarray(int[] nums) {
-        boolean deleted = false;
-        int maxCount = 0;
-        int right;
-        
-        for (right=0; right<nums.length; right++) {
-            if (nums[right] == 0) {
-                if (!deleted) {
-                    deleted = true;
-                    continue;
-                }
-                else break;
-            }
-            maxCount++;
-        }
-
-        int currCount = maxCount;
         int left = 0;
-        while (right < nums.length) {
-            if (nums[right] == 0 && deleted) {
-                if (nums[left++] == 0) {
-                    deleted = false;
-                    continue;
+        int zeroCount = 0;
+        int maxLen = 0;
+
+        for (int right = 0; right < nums.length; right++) {
+            if (nums[right] == 0) {
+                zeroCount++;
+            }
+
+            while (zeroCount > 1) {
+                if (nums[left] == 0) {
+                    zeroCount--;
                 }
-                currCount--;
+                left++;
             }
 
-            else if (nums[right] == 0) {
-                deleted = true;
-                right++;
-            }
-
-            else {
-                currCount++;
-                right++;
-            }
-            if (currCount > maxCount) maxCount = currCount;
+            maxLen = Math.max(maxLen, right - left);
         }
 
-        return deleted ? maxCount : (maxCount-1);
+        return maxLen;
     }
 }
